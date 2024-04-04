4383af0794d098fe808690f0f7cf00ac23430668
tlipoca9 <tlipoca9@gmail.com>
2024-04-04 16:10:36 +0800

    feat: add parser.TOML

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/onsi/ginkgo/v2 v2.17.1
 	github.com/onsi/gomega v1.32.0
+	github.com/pelletier/go-toml/v2 v2.0.8
 	github.com/pterm/pterm v0.12.79
 	mvdan.cc/sh/v3 v3.8.0
 )
@@ -48,7 +49,6 @@ require (
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
--- parser/common.go
+++ parser/common.go
@@ -3,36 +3,20 @@ package parser
 import (
 	"github.com/goccy/go-json"
 	"github.com/goccy/go-yaml"
+	"github.com/pelletier/go-toml/v2"
 )
 
-type CommonParserOptions struct {
-	UnmarshalFunc func([]byte, any) error
+// JSON is the shortcut for Func(json.Unmarshal)
+func JSON() Parser {
+	return Func(json.Unmarshal)
 }
 
-type CommonParser struct {
-	opt CommonParserOptions
+// YAML is the shortcut for Func(yaml.Unmarshal)
+func YAML() Parser {
+	return Func(yaml.Unmarshal)
 }
 
-func (p *CommonParser) WithUnmarshalFunc(f func([]byte, any) error) *CommonParser {
-	p.opt.UnmarshalFunc = f
-	return p
-}
-
-func (p *CommonParser) Unmarshal(b []byte, v any) error {
-	return p.opt.UnmarshalFunc(b, v)
-}
-
-// Common returns a new CommonParser
-func Common() *CommonParser {
-	return &CommonParser{}
-}
-
-// JSON is the shortcut for Common().WithUnmarshalFunc(json.Unmarshal)
-func JSON() *CommonParser {
-	return Common().WithUnmarshalFunc(json.Unmarshal)
-}
-
-// YAML is the shortcut for Common().WithUnmarshalFunc(yaml.Unmarshal)
-func YAML() *CommonParser {
-	return Common().WithUnmarshalFunc(yaml.Unmarshal)
+// TOML is the shortcut for Func(toml.Unmarshal)
+func TOML() Parser {
+	return Func(toml.Unmarshal)
 }
